935a9a328654741e0618216859c34d950cd4eae6
zengsj <zengshuangjie@gmail.com>
2019-11-14 09:01:03 +0800

    chore: add gitlab-ci image & runner

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,22 @@
+const isDev = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-  publicPath : process.env.NODE_ENV === 'production'
+  publicPath : isDev
     ? '/server/client/web_app/skyline/'
     : '/',
   chainWebpack : (config) => {
+    // hack [vue-cli-service serve] command
+    if (isDev) {
+      /* eslint-disable-next-line global-require */
+      const path = require('path');
+
+      // redirect to playground
+      config.entry('app')
+        .clear()
+        .add(path.resolve('./packages/playground/main.ts'));
+    }
+
+    // support markdown file
     const mdRule = config.module.rule('md').test(/\.md$/);
     const addLoader = ({ loader, options }) => {
       mdRule.use(loader).loader(loader).options(options);
