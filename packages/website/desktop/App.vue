b5ee14e3f8e4706029581e67ada4f2156ddc48a8
zengsj <zengshuangjie@gmail.com>
2019-11-29 17:04:21 +0800

    chore(website): website with logo

--- packages/website/desktop/App.vue
+++ packages/website/desktop/App.vue
@@ -1,20 +1,45 @@
 <template>
-  <div class="skyline">
-    hello skyline
-  </div>
+  <line-app>
+    <line-content color="dark">
+      <div class="logo">skyline</div>
+    </line-content>
+  </line-app>
 </template>
 
-<script>
-export default {
-};
+<script lang="ts">
+import Vue from 'vue';
+import { App } from '@/components/app';
+import { Header } from '@/components/header';
+import { Content } from '@/components/content';
+
+Vue.use(App);
+Vue.use(Header);
+Vue.use(Content);
+
+export default Vue.extend({
+  name : 'App',
+});
 </script>
 
-<style>
+<style lang="scss">
 
-.skyline {
-  position: relative;
+.logo {
+  display: flex;
+
+  align-items: center;
+  justify-content: center;
+
   width: 100%;
   height: 100%;
+
+  color: #fff;
+
+  font-family: "Montserrat";
+  font-size: 50px;
+
+  line-height: 40px;
+
+  text-transform: uppercase;
 }
 
 </style>
--- packages/website/desktop/main.ts
+++ packages/website/desktop/main.ts
@@ -1,7 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
+import '@/style/skyline.bundle.scss';
+import '@/style/animation.scss';
+import '@/themes/skyline.globals.scss';
+// import '@/themes/skyline.globals.ios.scss';
+
+Vue.config.productionTip = false;
 
 new Vue({
-  el     : '#app',
   render : h => h(App),
-}).$mount();
+}).$mount('#app');
