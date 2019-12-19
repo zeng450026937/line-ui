9c395de3172149d0013101c5bc2cb99ff06d279b
chenhf2 <chenhf2@yealink.com>
2019-12-19 09:40:25 +0800

    feat: range

---
+++ src/components/range/demo/index.vue
@@ -0,0 +1,58 @@
+<template>
+   <div>
+     <line-range v-model="value"></line-range>
+     <line-range v-model="value" pin></line-range>
+     <line-range v-model="dualValue" dual-knobs pin></line-range>
+     <line-range v-model="value" :step="10" snaps></line-range>
+     <line-range v-model="value" :step="10" snaps ticks></line-range>
+     {{value}}
+     {{dualValue}}
+   </div>
+</template>
+
+<script>
+import { Range as LineRange } from '@/components/range';
+
+export default {
+  name : 'RangeDemo',
+
+  components : {
+    LineRange,
+  },
+
+  props : {
+
+  },
+
+  data() {
+    return {
+      value     : 20,
+      dualValue : {},
+    };
+  },
+
+  computed : {
+
+  },
+
+  created() {
+
+  },
+
+  mounted() {
+
+  },
+
+  methods : {
+
+  },
+
+  watch : {
+
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
