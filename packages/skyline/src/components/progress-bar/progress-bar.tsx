dad73e2e061583c41c7459fbc0568dd56816e042
chenhf2 <chenhf2@yealink.com>
2020-03-06 14:18:07 +0800

    feat(progress-bar): add

---
+++ packages/skyline/src/components/progress-bar/progress-bar.tsx
@@ -0,0 +1,82 @@
+import { createNamespace } from 'skyline/utils/namespace';
+import { createColorClasses, useColor } from 'skyline/mixins/use-color';
+
+const { createComponent, bem } = /*#__PURE__*/ createNamespace('progress-bar');
+
+const clamp = (min: number, n: number, max: number) => {
+  return Math.max(min, Math.min(n, max));
+};
+
+const renderIndeterminate = (h: any) => {
+  return ([
+    <div class="indeterminate-bar-primary"><span class="progress-indeterminate"></span></div>,
+    <div class="indeterminate-bar-secondary"><span class="progress-indeterminate"></span></div>,
+  ]);
+};
+
+const renderProgress = (h: any, value: number, buffer: number) => {
+  const finalValue = clamp(0, value, 1);
+  const finalBuffer = clamp(0, buffer, 1);
+
+  return ([
+    <div class="progress" style={{ transform: `scaleX(${ finalValue })` }}></div>,
+    finalBuffer !== 1 && <div class="buffer-circles"></div>,
+    <div class="progress-buffer-bar" style={{ transform: `scaleX(${ finalBuffer })` }}></div>,
+  ]);
+};
+
+
+export default /*#__PURE__*/ createComponent({
+  mixins : [
+    /*#__PURE__*/ useColor(),
+  ],
+
+  props : {
+    type : {
+      type    : String,
+      default : 'determinate',
+    },
+    reversed : {
+      type    : Boolean,
+      default : false,
+    },
+    value : {
+      type    : Number,
+      default : 0,
+    },
+    buffer : {
+      type    : Number,
+      default : 1,
+    },
+  },
+
+  render(h) {
+    const {
+      mode, type, value, paused, reversed, buffer, color,
+    } = this;
+
+    return (
+      <div
+        role="progressbar"
+        aria-valuenow={type === 'determinate' ? value : null}
+        aria-valuemin="0"
+        aria-valuemax="1"
+        class={[
+          bem(),
+          {
+            ...createColorClasses(color),
+            [mode]                     : true,
+            [`progress-bar-${ type }`] : true,
+            'progress-paused'          : paused,
+            'progress-bar-reversed'    : document.dir === 'rtl' ? !reversed : reversed,
+          },
+        ]}
+      >
+      {type === 'indeterminate'
+        ? renderIndeterminate(h)
+        : renderProgress(h, value, buffer)
+      }
+      </div>
+    );
+  },
+});
---
+++ packages/skyline/src/components/progress-bar/usage.vue
@@ -0,0 +1,87 @@
+<template>
+  <line-content>
+    <line-list>
+      <line-list-header>
+        <line-label>Default</line-label>
+      </line-list-header>
+
+      <line-progress-bar></line-progress-bar>
+
+      <line-list-header>
+        <line-label>Default at 50%</line-label>
+      </line-list-header>
+
+      <line-progress-bar :value="0.5"></line-progress-bar>
+
+      <line-list-header>
+        <line-label>Color</line-label>
+      </line-list-header>
+
+      <!-- Progress Bar with Color -->
+      <line-progress-bar
+        color="secondary"
+        :value="0.5"
+      ></line-progress-bar>
+      <line-progress-bar
+        color="tertiary"
+        :value="0.8"
+      ></line-progress-bar>
+
+      <line-list-header>
+        <line-label>
+          Indeterminate
+        </line-label>
+      </line-list-header>
+      <line-progress-bar type="indeterminate"></line-progress-bar>
+
+      <line-list-header>
+        <line-label>
+          Indeterminate (reversed)
+        </line-label>
+      </line-list-header>
+      <line-progress-bar
+        type="indeterminate"
+        :reversed="true"
+      ></line-progress-bar>
+
+      <line-list-header>
+        <line-label>
+          Buffer
+        </line-label>
+      </line-list-header>
+      <line-progress-bar
+        :value="0.25"
+        :buffer="0.5"
+      ></line-progress-bar>
+    </line-list>
+  </line-content>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      value : 0,
+    };
+  },
+
+  methods : {
+
+  },
+
+  mounted() {
+    setInterval(() => {
+      this.value = this.value >= 1 ? 0 : this.value + 0.1;
+    }, 1000);
+  },
+
+});
+</script>
+
+<style lang="scss" scoped>
+.line-progress-bar {
+  margin: 10px 0;
+}
+</style>
