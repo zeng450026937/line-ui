c00e16704d8a69e8a4fd6138d5e4533787e109bb
chenhf2 <chenhf2@yealink.com>
2020-03-11 14:12:52 +0800

    feat(infinite-scroll): add

---
+++ packages/skyline/src/components/infinite-scroll-content/index.ts
@@ -0,0 +1,8 @@
+// Auto Generated
+// This file is auto generated, do not modify directly.
+
+import InfiniteScrollContent from 'skyline/components/infinite-scroll-content/infinite-scroll-content';
+
+export {
+  InfiniteScrollContent,
+};
---
+++ packages/skyline/src/components/infinite-scroll/index.ts
@@ -0,0 +1,8 @@
+// Auto Generated
+// This file is auto generated, do not modify directly.
+
+import InfiniteScroll from 'skyline/components/infinite-scroll/infinite-scroll';
+
+export {
+  InfiniteScroll,
+};
