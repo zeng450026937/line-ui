517509bc7463d6b175defe96b61779e8395b9266
zengsj <zengshuangjie@gmail.com>
2020-03-11 10:53:29 +0800

    chore: specific process.env.TARGET will load packages babel.config.js if exsits

--- vue-plugins/development.js
+++ vue-plugins/development.js
@@ -4,35 +4,45 @@ module.exports = (api, options) => {
       const packagesDir = api.resolve('packages');
       const packageDir = `${ packagesDir }/${ process.env.TARGET }`;
 
-      config.module.rule('js')
-        .use('babel-loader')
-        .tap(options => {
-          return {
-            ...options,
-            babelrc  : false,
-            root     : packageDir,
-            rootMode : 'upward',
-          };
-        });
+      const tapBabel = (rule) => {
+        config.module.rule(rule)
+          .use('babel-loader')
+          .tap((options) => {
+            return {
+              ...options,
+              babelrc  : false,
+              root     : packageDir,
+              rootMode : 'upward',
+            };
+          });
+      };
+
+      if (api.hasPlugin('babel')) {
+        tapBabel('js');
+
+        if (api.hasPlugin('typescript')) {
+          tapBabel('ts');
+        }
+      }
     }
+
+    // for development, set "skyline" alias to source code
+    // config.resolve.alias
+    //   .set('skyline', api.resolve('packages/skyline/src'));
+
+    // auto import
+    // config.module.rule('vue')
+    //   .use('skyline-auto-import')
+    //   .loader(api.resolve('packages/webpack-loader-auto-import/src/index.js'))
+    //   // check kebab case
+    //   .options('kebab')
+    //   .after('vue-loader');
+
+    // config.module.rule('import')
+    //   .resourceQuery('blockType=import')
+    //   .use('skyline-auto-import')
+    //   .loader(api.resolve('packages/webpack-loader-auto-import/src/import.js'))
+    //   .after('babel-loader')
+    //   .end();
   });
-
-  // for development, set "skyline" alias to source code
-  // config.resolve.alias
-  //   .set('skyline', api.resolve('packages/skyline/src'));
-
-  // auto import
-  // config.module.rule('vue')
-  //   .use('skyline-auto-import')
-  //   .loader(api.resolve('packages/webpack-loader-auto-import/src/index.js'))
-  //   // check kebab case
-  //   .options('kebab')
-  //   .after('vue-loader');
-
-  // config.module.rule('import')
-  //   .resourceQuery('blockType=import')
-  //   .use('skyline-auto-import')
-  //   .loader(api.resolve('packages/webpack-loader-auto-import/src/import.js'))
-  //   .after('babel-loader')
-  //   .end();
 };
