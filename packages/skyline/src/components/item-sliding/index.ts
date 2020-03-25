b55a34f99304a47a579a88900819882ad466be90
chenhf2 <chenhf2@yealink.com>
2020-03-25 17:11:53 +0800

    feat(item-sliding): add

---
+++ packages/skyline/src/components/item-sliding/index.ts
@@ -0,0 +1,8 @@
+// Auto Generated
+// This file is auto generated, do not modify directly.
+
+import ItemSliding from 'skyline/src/components/item-sliding/item-sliding';
+
+export {
+  ItemSliding,
+};
