b0fde607d1476079dcb2956a6adf85fa84d56219
zengsj <zengshuangjie@gmail.com>
2020-04-09 15:01:33 +0800

    wip(vue-cli-line): initial add

--- packages/vue-cli-plugin-line/index.js
+++ packages/vue-cli-plugin-line/index.js
@@ -1,10 +1,55 @@
 module.exports = (api, options) => {
-  // TBD
-  // auto import
-  // config.module.rule('vue')
-  //   .use('line-auto-import')
-  //   .loader(require.resolve('@line-ui/webpack-loader-auto-import'))
-  //   // check kebab case
-  //   .options('kebab')
-  //   .after('vue-loader');
+  api.chainWebpack((config) => {
+    // line config
+    const line = {
+      autoimport: true,
+      svgsprite: true,
+      svgcomponent: false,
+      i18n: true,
+    };
+
+    if (line.autoimport) {
+      config.module
+        .rule('vue')
+        .use('line-auto-import')
+        .loader(require.resolve('@line-ui/webpack-loader-auto-import'))
+        .options(require('@line-ui/line/import.config'))
+        .after('vue-loader');
+    }
+
+    if (line.svgsprite) {
+      config.module.rules.delete('svg');
+
+      config.module
+        .rule('svg')
+        .test(/\.(svg)(\?.*)?$/)
+        .use('svg-sprite')
+        .loader(
+          require.resolve('@line-ui/webpack-plugin-svg-sprite/lib/loader')
+        );
+
+      config
+        .plugin('svg-sprite')
+        .use(require('@line-ui/webpack-plugin-svg-sprite'));
+    } else if (line.svgcomponent) {
+      if (api.hasPlugin('babel')) {
+        config.module.rules.delete('svg');
+
+        config.module
+          .rule('svg')
+          .test(/\.(svg)(\?.*)?$/)
+          // .use('babel-loader')
+          // .loader(require.resolve('babel-loader'))
+          // .end()
+          .use('svg-component')
+          .loader(require.resolve('@line-ui/webpack-loader-svg-component'));
+      } else {
+        console.log('svg-component require babel-loader with jsx support.');
+      }
+    }
+
+    if (line.i18n) {
+      config.plugin('i18n').use(require('@line-ui/webpack-plugin-i18n'));
+    }
+  });
 };
